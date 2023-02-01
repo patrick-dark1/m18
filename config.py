faa81305287485d45d456242d7c831558fd21fcc
patrick-dark1 <116102838+patrick-dark1@users.noreply.github.com>
2023-02-01 19:09:58 +0300

    Add files via upload
    Модуль 18 - Telegram Bot Converter

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '5913718193:AAG6k2JE0FvS0Hqcli0PQFz7nNUkNyDXVtE'
+keys: dict[str, str] = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
